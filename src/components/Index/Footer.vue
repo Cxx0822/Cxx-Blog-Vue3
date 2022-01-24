4bc81a30743aa74426e71e71f586bf72678a9091
Chenxiuxiang <chenxiuxiang@estun.com>
2022-01-24 12:07:58 +0800

    增加主页Footer组件

---
+++ src/components/Index/Footer.vue
@@ -0,0 +1,87 @@
+<template>
+  <!--网站底部-->
+  <div class="footer">
+    <el-row :gutter="24" style="padding-top: 30px">
+      <el-col :sm="8">
+        <el-row :gutter="24" class="footer-title">
+          <el-col :span="4" :offset="7">联系博主</el-col>
+        </el-row>
+        <el-row :gutter="24" class="footer-content">
+          <el-col :sm="10" :offset="4">
+            QQ:{{ personalInfo.QQ }}
+            <br>
+            邮箱:{{ personalInfo.Email }}
+          </el-col>
+        </el-row>
+      </el-col>
+
+      <el-col :sm="8">
+        <el-row :gutter="24" class="footer-title">
+          <el-col :span="24">签名</el-col>
+        </el-row>
+        <el-row :gutter="24" class="footer-content">
+          <el-col :span="24" style="font-size: 15px">
+            {{ footerMotto }}
+          </el-col>
+        </el-row>
+      </el-col>
+      <el-col :sm="8">
+        <el-row :gutter="24" class="footer-title">
+          <el-col :span="24">网站信息</el-col>
+        </el-row>
+        <el-row :gutter="24" class="footer-content">
+          <el-col :span="24">
+            总访问量：{{ websiteCount.pv }} <br>
+            访客量: {{ websiteCount.uv }}
+          </el-col>
+        </el-row>
+      </el-col>
+
+    </el-row>
+    <el-divider ></el-divider>
+
+    <el-row :gutter="24" style="height: 50px;margin-top: 25px;font-size: 12px">
+      <el-col :span="24">
+        Copyright&nbsp;&copy;&nbsp;2021-2022&nbsp;Skymo's&nbsp;Blog.&nbsp;All&nbsp;rights&nbsp;reserved.&nbsp;
+      </el-col>
+    </el-row>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+import { reactive, ref } from 'vue'
+
+const personalInfo = reactive({
+  QQ: '1556464090',
+  Email: '1556464090@qq.com'
+})
+
+const footerMotto = ref('青春是一个短暂的美梦, 当你醒来时, 它早已消失无踪')
+
+const websiteCount = reactive({
+  pv: 0,
+  uv: 0
+})
+
+</script>
+
+<style scoped>
+.footer {
+  margin: 0 auto;
+  text-align: center;
+  min-height: 20px;
+  max-width: 100%;
+  padding: 0 15px;
+}
+
+.footer-title {
+  font-size: 13px;
+  font-weight: bold;
+  margin-bottom: 5px;
+}
+
+.footer-content {
+  font-size: 13px;
+}
+</style>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,39 +1,47 @@
 <template>
-  <!-- 首页照片组件 -->
-  <HeadPhoto v-if="$route.name==='Home'" @scrollIndex="scrollIndex"></HeadPhoto>
+  <div class="index-container">
+    <!-- 首页照片组件 -->
+    <HeadPhoto v-if="$route.name==='Home'" @scrollIndex="scrollIndex"></HeadPhoto>
 
-  <!-- 使用弹性盒子实现左中右结构 -->
-  <div class="index-main" id="index" >
-    <!-- 左侧区域 -->
-    <div class="index-main-column-left">
-      <Card></Card>
-    </div>
+    <!-- 使用弹性盒子实现左中右结构 -->
+    <div class="index-main" id="index" >
+      <!-- 左侧区域 -->
+      <div class="index-main-column-left">
+        <Card></Card>
+      </div>
 
-    <!-- 中间主体区域 -->
-    <div class="index-main-column-middle">
-      <!-- 路由显示 -->
-      <!-- 这里为路由定义中 index路由的子路由 -->
-      <router-view/>
-    </div>
+      <!-- 中间主体区域 -->
+      <div class="index-main-column-middle">
+        <!-- 路由显示 -->
+        <!-- 这里为路由定义中 index路由的子路由 -->
+        <router-view/>
+      </div>
 
-    <!-- 右侧区域 -->
-    <div class="index-main-column-right">
-      <RightCard></RightCard>
-      <Calendar></Calendar>
+      <!-- 右侧区域 -->
+      <div class="index-main-column-right">
+        <RightCard></RightCard>
+        <Calendar></Calendar>
+      </div>
     </div>
 
     <!--回到顶部-->
     <el-backtop style="box-shadow: none;background: none;">
-      <img alt="" src="@/assets/toTopLogo.webp" style="width: 40px;height: 40px;">
+      <img alt="" src="../assets/toTopLogo.webp" style="width: 40px;height: 40px;">
     </el-backtop>
+
+    <!--底部footer-->
+    <Footer class="index-foot"></Footer>
   </div>
+
 </template>
 
 <script setup lang="ts">
+// 引入子组件
 import HeadPhoto from '@/components/Index/HeadPhoto.vue'
 import Card from '@/components/Index/Card.vue'
 import RightCard from '@/components/Index/RightCard.vue'
 import Calendar from '@/components/Index/Calendar.vue'
+import Footer from '@/components/Index/Footer.vue'
 
 // 将元素滚动到浏览器窗口的可见区域
 const scrollIndex = () => {
@@ -46,6 +54,13 @@ const scrollIndex = () => {
 </script>
 
 <style lang="less" scoped>
+.index-container {
+  min-height: 100vh;
+  // background-image: url("https://cdn.jsdelivr.net/gh/yubifeng/blog-resource/bloghosting/2021/04/23/first87.png")!important; ;
+  background-repeat: repeat-x;
+  background-attachment:fixed;
+  background-size:cover;
+}
 .index-main {
   display: flex;
   margin-top: 70px;
@@ -53,7 +68,6 @@ const scrollIndex = () => {
   padding-top: 20px;
   margin-left: auto !important;
   margin-right: auto !important;
-
   opacity: 0.97;
 
   .index-main-column-left{
@@ -73,5 +87,11 @@ const scrollIndex = () => {
     margin-left: 20px;
     margin-right: 30px;
   }
+}
+
+.index-foot {
+  background-color: #333333;
+  color: white!important;
+  opacity: 0.9;
 }
 </style>
